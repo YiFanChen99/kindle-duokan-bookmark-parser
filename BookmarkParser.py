60f4180d5d7b344bcd879d2b973f18f9ae88a747
Ricky <pig211@gmail.com>
2014-04-19 21:40:07 +0800

    Show the book name Add gitignore

--- BookmarkParser.py
+++ BookmarkParser.py
@@ -4,8 +4,8 @@
 將多看系統標記的批注彙整並輸出，以供用電腦編輯原始檔案
 '''
 
+import os.path
 from lxml import etree
-from io import StringIO, BytesIO
 
 def print_BookContent(element):
     print '< 文字:', element.text,
@@ -13,8 +13,12 @@ def print_BookContent(element):
 def print_ChapterID(element):
     print ', 章節:', element.text, ">"
 
-file_path = u'dkx1'
+file_path = u'dkx'
 root = etree.parse(file_path)
+
+# 檔案對應的書名
+file_item = root.find('FileItem')
+print os.path.basename(file_item.attrib['FilePath']), "\n"
 
 for element in root.iter():
     if element.tag == 'BookContent':
