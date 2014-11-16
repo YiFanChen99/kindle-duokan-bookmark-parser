6cfae2119682a3493947b1e86187e661aa57cb3f
RickyChen <pig211@gmail.com>
2014-11-16 19:26:59 +0800

    修改成能一次拖放多個檔案。 而輸出會寫到檔案 contexts 中。

--- BookmarkParser.py
+++ BookmarkParser.py
@@ -1,41 +1,58 @@
 # -*- coding:utf-8 -*-
 
 '''
-將多看系統標記的批注彙整並輸出，以供用電腦編輯原始檔案
+將多看系統標記的批注彙整並輸出到檔案，以供用電腦編輯原始檔案
 '''
 
 import os.path
+import sys
 from collections import defaultdict
 from lxml import etree
+OUTPUT_FILE = 'D:\Dropbox\SyncWithPig\kindle-duokan-bookmark-parser\contents.txt'
 #print etree.LXML_VERSION #lxml版本訊息
 
-infos = defaultdict(list)
 
-def add_highlight_info(highlight):
-    book_content = highlight.find('BookContent').text
-    chapter_id = highlight.find('BeginPos').find('ChapterID').text
-    
-    contents = infos[chapter_id]
-    contents.append(book_content)
-    
-file_path = u'dkx'
-root = etree.parse(file_path)
+def parse_file(file_path):
+    infos = defaultdict(list)
 
-# 檔案對應的書名
-file_item = root.find('FileItem')
-print os.path.basename(file_item.attrib['FilePath']), "\n"
+    def add_highlight_info(highlight):
+        book_content = highlight.find('BookContent').text
+        chapter_id = highlight.find('BeginPos').find('ChapterID').text
 
-# 彙整每一個高亮的資訊
-highlights = root.findall('.//ReadingDataItem')
-for highlight in highlights:
-    add_highlight_info(highlight)
+        contents = infos[chapter_id]
+        contents.append(book_content)
+
+    root = etree.parse(file_path.encode('utf-8'))
+
+    # 檔案對應的書名
+    book_name = os.path.basename(root.find('FileItem').attrib['FilePath'])
+
+    # 彙整每一個高亮的資訊
+    highlights = root.findall('.//ReadingDataItem')
+    for highlight in highlights:
+        add_highlight_info(highlight)
+
+    save_contents(book_name, infos)
+
 
 # 將章節與其內文輸出
-for key, value in sorted(infos.items()):
-    print key, ':'
-    for content in value:
-        print '  <', content , '>',
-    print ''
+def save_contents(book_name, infos):
+    with open(OUTPUT_FILE, 'a') as file:
+        results = u''
+        for chapter, contents in sorted(infos.items()):
+            results += u'\t' + chapter + u':\n'
+            results += u'\t\t  「 ' + u' 」\t「 '.join(contents) + u' 」\n'
+        file.write(book_name.encode('utf-8') + ' 已經完成。\n')
+        file.write(results.encode('utf-8') + '\n\n')
 
-print u'完成，請按Enter終止。'
-raw_input('')
+
+def main():
+    for file_id in range(1, len(sys.argv)):
+        parse_file(sys.argv[file_id])
+
+
+if __name__ == "__main__":
+    main()
+    print u'完成，請按 Enter 檢視檔案。'
+    raw_input('')
+    os.startfile(OUTPUT_FILE)
---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+
+語言：Python 2.7.x
+
+需要安裝 module：
+  lxml （可用 easy_install 下載）
+  
+拖放檔案時路徑若有中文名稱可能出錯。
