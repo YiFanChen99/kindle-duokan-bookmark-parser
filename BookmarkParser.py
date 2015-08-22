12c0efd9c4fb58fc7b590ba2252509c6fa8dd05a
RickyChen <pig211@gmail.com>
2015-08-22 16:17:55 +0800

    Convert to python 3. Update ReadMe.

--- BookmarkParser.py
+++ BookmarkParser.py
@@ -1,28 +1,26 @@
-# -*- coding:utf-8 -*-
-
-'''
+"""
 將多看系統標記的批注彙整並輸出到檔案，以供用電腦編輯原始檔案
-'''
+"""
 
 import os.path
 import sys
 from collections import defaultdict
 from lxml import etree
-OUTPUT_FILE = os.path.dirname(sys.argv[0]) + '/contents.txt'
+OUTPUT_FILE = str.format('{0}{1}{2}', os.path.dirname(sys.argv[0]), os.path.sep, 'contents.txt')
 #print etree.LXML_VERSION #lxml版本訊息
 
 
 def parse_file(file_path):
     infos = defaultdict(list)
 
-    def add_highlight_info(highlight):
-        book_content = highlight.find('BookContent').text
-        chapter_id = highlight.find('BeginPos').find('ChapterID').text
+    def add_highlight_info(the_highlight):
+        book_content = the_highlight.find('BookContent').text
+        chapter_id = the_highlight.find('BeginPos').find('ChapterID').text
 
         contents = infos[chapter_id]
         contents.append(book_content)
 
-    root = etree.parse(file_path.encode('utf-8'))
+    root = etree.parse(file_path)
 
     # 檔案對應的書名
     book_name = os.path.basename(root.find('FileItem').attrib['FilePath'])
@@ -37,13 +35,13 @@ def parse_file(file_path):
 
 # 將章節與其內文輸出
 def save_contents(book_name, infos):
-    with open(OUTPUT_FILE, 'a') as the_file:
-        results = u''
+    with open(OUTPUT_FILE, 'a', encoding='utf-8') as the_file:
+        results = ''
         for chapter, contents in sorted(infos.items()):
-            results += u'\t' + chapter + u':\n'
-            results += u'\t\t  「 ' + u' 」\t「 '.join(contents) + u' 」\n'
-        the_file.write(book_name.encode('utf-8') + ' 已經完成。\n')
-        the_file.write(results.encode('utf-8') + '\n\n')
+            results += str.format('\t{0}:\n', chapter)
+            results += str.format('\t\t  「 {0} 」\n', ' 」\t「 '.join(contents))
+        print(str.format('{0} 已經完成。\n', book_name), file=the_file)
+        print(str.format('{0}\n\n', results), file=the_file)
 
 
 def main():
@@ -53,6 +51,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-    print u'完成，請按 Enter 檢視檔案。'
-    raw_input('')
+    print('完成，請按 Enter 檢視檔案。')
+    input('')
     os.startfile(OUTPUT_FILE)
---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+
+語言：Python 3
+
+需要安裝 module：
+	lxml （Windows 可於 [這裡](http://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml) 下載對應的 whl 檔後，以 pip 安裝。）
+	
+拖放檔案時路徑若有 中文 / 空白 可能出錯。
